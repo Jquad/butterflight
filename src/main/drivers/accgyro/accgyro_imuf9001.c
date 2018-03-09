19f22959df4b97c0c8f552846b949e509d435062
Tim Sweet <timothyswt@gmail.com>
2018-03-09 09:22:09 -0700

    removing R value settings

--- src/main/drivers/accgyro/accgyro_imuf9001.c
+++ src/main/drivers/accgyro/accgyro_imuf9001.c
@@ -234,9 +234,9 @@ void imufSpiGyroInit(gyroDev_t *gyro)
 
     rxData.param1 = VerifyAllowedCommMode(gyroConfig()->imuf_mode);
     rxData.param2 = 1; //does nothing, place holder for loop speed
-    rxData.param3 = ( (uint16_t)gyroConfig()->imuf_pitch_q << 16 ) | (uint16_t)gyroConfig()->imuf_pitch_r;
-    rxData.param4 = ( (uint16_t)gyroConfig()->imuf_roll_q << 16 ) | (uint16_t)gyroConfig()->imuf_roll_r;
-    rxData.param5 = ( (uint16_t)gyroConfig()->imuf_yaw_q << 16 ) | (uint16_t)gyroConfig()->imuf_yaw_r;
+    rxData.param3 = ( (uint16_t)gyroConfig()->imuf_pitch_q << 16);
+    rxData.param4 = ( (uint16_t)gyroConfig()->imuf_roll_q << 16);
+    rxData.param5 = ( (uint16_t)gyroConfig()->imuf_yaw_q << 16);
     rxData.param6 = ( (uint16_t)gyroConfig()->imuf_pitch_lpf_cutoff_hz << 16) | (uint16_t)gyroConfig()->imuf_roll_lpf_cutoff_hz;
     rxData.param7 = ( (uint16_t)gyroConfig()->imuf_yaw_lpf_cutoff_hz << 16) | (uint16_t)gyroConfig()->imuf_dyn_gain;
     rxData.param8 = ( (int16_t)boardAlignment()->rollDegrees << 16 ) | returnGyroAlignmentForImuf9001();
