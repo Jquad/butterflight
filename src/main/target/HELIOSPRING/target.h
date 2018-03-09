8323eae73b14c1f61e32045e161d04ced4af52ad
Tim Sweet <timothyswt@gmail.com>
2018-03-08 18:19:38 -0700

    removing chnge to moron threshold and ensuring values are defined

--- src/main/target/HELIOSPRING/target.h
+++ src/main/target/HELIOSPRING/target.h
@@ -187,6 +187,11 @@
 
 #define CAMERA_CONTROL_PIN             PB6    // define dedicated camera_osd_control pin
 
+#define HELIO_PROFILE_PITCH_Q 3500
+#define HELIO_PROFILE_ROLL_Q 3500
+#define HELIO_PROFILE_YAW_Q 2500
+#define HELIO_PROFILE_DYN_GAIN 10
+
 
 #if defined(HELIO_RACE)
 #define HELIO_PROFILE_PITCH_Q 800
@@ -203,9 +208,4 @@
 #define HELIO_PROFILE_ROLL_Q 2000
 #define HELIO_PROFILE_YAW_Q 1000
 #define HELIO_PROFILE_DYN_GAIN 50
-#else
-#define HELIO_PROFILE_PITCH_Q 3500
-#define HELIO_PROFILE_ROLL_Q 3500
-#define HELIO_PROFILE_YAW_Q 2500
-#define HELIO_PROFILE_DYN_GAIN 10
 #endif
